de8a55c096e96f270d71acd60aa11699e5a04368
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-10-22 19:56:09 +0300

    added sanity schemas

--- sanity/schemas/social.js
+++ sanity/schemas/social.js
@@ -1,17 +1,19 @@
 export default {
-  name: 'category',
-  title: 'Category',
+  name: 'social',
+  title: 'Social',
   type: 'document',
   fields: [
     {
       name: 'title',
       title: 'Title',
+      description: "Platform for social media",
       type: 'string',
     },
     {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
+      name: 'url',
+      title: 'Url',
+      type: 'url',
     },
+
   ],
 }
